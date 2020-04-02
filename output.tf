d6193a8338a40054255b3145cd6e0574efc1c6c6
RiceBowlJr <alexandre.kervadec@gmail.com>
2020-04-02 10:40:00 +0200

    chore(Outputs): add distribution DN and Zone ID

--- output.tf
+++ output.tf
@@ -2,3 +2,11 @@ output "cache_invalidation_command" {
   description = "CloudFront edge cache invalidation command. /path/to/invalidation/resource is like /index.html /error.html"
   value = "aws cloudfront create-invalidation --profile ${var.aws["profile"]} --distribution-id ${aws_cloudfront_distribution.web_dist.id} --paths /path/to/invalidation/resource"
 }
+output "distribution_domain_name" {
+  description = "CloudFront distribution domain name"
+  value       = aws_cloudfront_distribution.web_dist.domain_name
+}
+output "distribution_zone_id" {
+  description = "CloudFront distribution zone ID"
+  value       = aws_cloudfront_distribution.web_dist.hosted_zone_id
+}
